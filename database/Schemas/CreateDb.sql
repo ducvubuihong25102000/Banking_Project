0853787a55717c4ac08eb70d81d4699f90b1ac5f
Duc toc dai <ducvubuihong25102000@gmail.com>
2023-06-19 23:21:40 +0700

    [Done] Define Schema/ [Undone] Define Staging Table

---
+++ database/Schemas/CreateDb.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE [Bank]
+GO
+USE [Bank]
+GO
---
+++ database/Schemas/TrsSchema.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA [Trs] Authorization [dbo];
+GO
+
+SELECT sch.name AS [Schema_name], schema_id, u.name AS [Owner_name]
+FROM sys.schemas sch 
+INNER JOIN sys.sysusers u ON sch.principal_id = u.uid
